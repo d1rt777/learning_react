ef1c5377508f6d6fc78dd28c1b227a341601474b
d1rt777 <sarkozz161@gmail.com>
2025-10-06 21:39:56 +0300

    установка шаблона npx

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// ReactDOM.render(<h1>penis</h1>, document.getElementById('app'))
-
-const inputEnter = () => console.log('input enter');
-const mouseOver = () => console.log('mouse over');
-
-const inp = <input placeholder="help text" onClick={inputEnter} onMouseEnter = {mouseOver} />
-const app = document.getElementById('app')
-
-ReactDOM.render(inp, app, )
