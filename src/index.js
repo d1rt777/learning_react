0604dbdc1bd251f83a07a22496c1fbfb97e78b97
d1rt777 <sarkozz161@gmail.com>
2025-10-08 23:01:43 +0300

    правильное написание и обращение к ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import * as ReactDOMCLient from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const inputEnter = () => console.log('input enter');
+const mouseOver = () => console.log('mouse over');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const inp = <input placeholder="help text" onClick={inputEnter} onMouseEnter = {mouseOver} />
+
+const app = ReactDOMCLient.createRoot(document.getElementById('root'));
+
+app.render(inp)
