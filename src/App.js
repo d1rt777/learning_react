2196606220ee4a344d164e71c7d66f2501802825
d1rt777 <sarkozz161@gmail.com>
2025-10-13 20:27:35 +0300

    прошел 5 урок, написал поле принимающее значение с инпута

--- src/App.js
+++ src/App.js
@@ -4,24 +4,40 @@ import Image from './components/Image'
 import logo from './img/react.jpg'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            helpText: "help text",
+            userData: "",
+        }
+
+        this.inputClick = this.inputClick.bind(this)
+    }
+
+
     helpText = "help text"
     render() {
         return (
             <div className='name'>
-                <Header title="шапка сайта йоу"/>
-                <h1>{this.helpText}</h1>
+                <Header title="шапка сайта йоу" />
+                <h1>{this.state.helpText}</h1>
+                <h2>{this.state.userData}</h2>
+                <input placeholder={this.state.helpText}
+                    onChange={event => this.setState({ userData: event.target.value })}
+                    onClick={this.inputClick} onMouseEnter={this.mouseOver} />
 
-                <input placeholder={this.helpText} onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-
-                <p>{this.helpText === "help text" ? "yes" : "no"}</p>
-                <Image image = {logo}/>
+                <p>{this.state.helpText === "help text" ? "yes" : "no"}</p>
+                <Image image={logo} />
             </div>
         )
     }
 
 
-    inputClick (){console.log("you clicked");}
-    mouseOver (){console.log("you over");}
+    inputClick() {
+        this.setState({ helpText: "change" })
+        console.log("you clicked");
+    }
+    mouseOver() { console.log("you over"); }
 }
 
 export default App
